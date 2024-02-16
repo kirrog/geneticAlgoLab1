dafc79614fe171a407656dcc5159cd17b1c1e67d
kirrog <kirrog@zertan.ru>
2024-02-16 09:57:08 +0300

    [init] Completed lab

---
+++ main.py
@@ -0,0 +1,88 @@
+import time
+import tracemalloc
+from random import randint
+from typing import Tuple, Union, Any
+from numba.typed import List
+
+from numba import njit
+
+
+def check_sort(input_data, algo_func, njit_) -> Tuple[Union[bool, Any], float, int]:
+    sorted_data_reference = input_data.copy()
+    sorted_data_reference.sort()
+    if njit_:
+        input_data_new = List()
+        [input_data_new.append(x) for x in input_data]
+        input_data = input_data_new
+    tracemalloc.start()
+    start = time.time()
+    sorted_data_func = algo_func(input_data)
+    stop = time.time()
+    current, maximum = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
+    if njit_:
+        sorted_data_func = list(sorted_data_func)
+    return sorted_data_func == sorted_data_reference, stop - start, maximum
+
+
+def sort_default(list2sort):
+    max_num = max(list2sort)
+    min_num = min(list2sort)
+    cnt = [0] * (max_num + 1 - min_num)
+    for item in list2sort:
+        cnt[item - min_num] += 1
+    result = []
+    for num, count in enumerate(cnt):
+        for i in range(count):
+            result.append(num + min_num)
+    return result
+
+
+@njit
+def sort_optimised(list2sort):
+    max_num = max(list2sort)
+    min_num = min(list2sort)
+    cnt = [0] * (max_num + 1 - min_num)
+    for item in list2sort:
+        cnt[item - min_num] += 1
+    result = []
+    for num, count in enumerate(cnt):
+        for i in range(count):
+            result.append(num + min_num)
+    return result
+
+
+def generate_random_data(bias=0, up_level=1000, count=1000):
+    return [randint(bias, bias + up_level) for r in range(count)]
+
+
+iteration_num = 100
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    with open("results.tsv", "w") as f:
+        f.write(f"up_level_iter\tcount_iter\tdefault_accum\toptimised_accum\n")
+        for up_level_iter in [x * 1000 for x in range(1, 11)]:
+            for count_iter in [x * 1000 for x in range(1, 11)]:
+                default_time_accum = 0.0
+                default_mem_accum = 0.0
+                optimised_time_accum = 0.0
+                optimised_mem_accum = 0.0
+                for i in range(iteration_num):
+                    data = generate_random_data(0, up_level_iter, count_iter)
+                    check, time_, maximum_mem = check_sort(data, sort_default, False)
+                    assert check
+                    default_time_accum += time_ / iteration_num
+                    default_mem_accum += maximum_mem / iteration_num
+
+                    data = generate_random_data(0, up_level_iter, count_iter)
+                    check, time_, maximum_mem = check_sort(data, sort_optimised, True)
+                    assert check
+                    optimised_time_accum += time_ / iteration_num
+                    optimised_mem_accum += maximum_mem / iteration_num
+                f.write(f"{up_level_iter:05d}\t{count_iter:05d}\t"
+                        f"{default_time_accum:.09f}\t{optimised_time_accum:.09f}\t"
+                        f"{default_mem_accum:.01f}\t{optimised_mem_accum:.01f}\n")
+                print(f"{up_level_iter:05d}\t{count_iter:05d}\t{default_time_accum:.09f}\t"
+                      f"{optimised_time_accum:.09f}\t"
+                      f"{default_mem_accum:.01f}\t{optimised_mem_accum:.01f}")
---
+++ plots_creating.py
@@ -0,0 +1,90 @@
+from matplotlib import pyplot as plt
+
+if __name__ == "__main__":
+    with open("results.tsv", "r") as f:
+        data = [x.split("\t") for x in f.read().split("\n")][1:-1]
+
+        # Time heatmap
+        heatmap_time = [[0.0] * 10 for x in range(10)]
+        maximum_time = max(max([float(x[2]) for x in data]), max([float(x[3]) for x in data]))
+        minimum_time = min(min([float(x[2]) for x in data]), min([float(x[3]) for x in data]))
+        time_range = maximum_time - minimum_time
+        for up_level_iter, count_iter, default_time_accum, optimised_time_accum, default_mem_accum, optimised_mem_accum in data:
+            up_i = int(int(up_level_iter) / 1000) - 1
+            count_i = int(int(count_iter) / 1000) - 1
+            heatmap_time[up_i][count_i] = (float(default_time_accum) - float(optimised_time_accum)) / time_range
+        plt.imshow(heatmap_time)
+        plt.colorbar()
+        plt.title("Heatmap of time difference")
+        plt.savefig("Heatmap of time difference")
+        # plt.show()
+        plt.close()
+
+        # Memory heatmap
+        heatmap_mem = [[0.0] * 10 for x in range(10)]
+        maximum_mem = max(max([float(x[4]) for x in data]), max([float(x[5]) for x in data]))
+        minimum_mem = min(min([float(x[4]) for x in data]), min([float(x[5]) for x in data]))
+        time_range = maximum_mem - minimum_mem
+        for up_level_iter, count_iter, default_time_accum, optimised_time_accum, default_mem_accum, optimised_mem_accum in data:
+            up_i = int(int(up_level_iter) / 1000) - 1
+            count_i = int(int(count_iter) / 1000) - 1
+            heatmap_mem[up_i][count_i] = (float(default_mem_accum) - float(optimised_mem_accum)) / time_range
+        plt.imshow(heatmap_mem)
+        plt.colorbar()
+        plt.title("Heatmap of memory difference")
+        plt.savefig("Heatmap of memory difference")
+        # plt.show()
+        plt.close()
+
+        # Plot of times
+        for up_ in ["01000", "10000"]:
+            x = [x[1] for x in data if x[0] == up_]
+            y_d = [float(x[2]) for x in data if x[0] == up_]
+            y_o = [float(x[3]) for x in data if x[0] == up_]
+            plt.plot(x, y_d, 'g', label="default")
+            plt.plot(x, y_o, 'b', label="optimised")
+            plt.xlabel("n")
+            plt.ylabel("Average time")
+            plt.legend()
+            plt.title(f"Plot time def up lvl {up_}")
+            plt.savefig(f"Plot time def up lvl {up_}")
+            # plt.show()
+            plt.close()
+        for count_ in ["01000", "10000"]:
+            x = [x[0] for x in data if x[1] == count_]
+            y_d = [float(x[2]) for x in data if x[1] == count_]
+            y_o = [float(x[3]) for x in data if x[1] == count_]
+            plt.plot(x, y_d, 'g', label="default")
+            plt.plot(x, y_o, 'b', label="optimised")
+            plt.xlabel("k")
+            plt.ylabel("Average time")
+            plt.title(f"Plot time def count {count_}")
+            plt.savefig(f"Plot time def count {count_}")
+            # plt.show()
+            plt.close()
+
+        # plot of memory
+        for up_ in ["01000", "10000"]:
+            x = [x[1] for x in data if x[0] == up_]
+            y_d = [float(x[4]) for x in data if x[0] == up_]
+            y_o = [float(x[5]) for x in data if x[0] == up_]
+            plt.plot(x, y_d, 'g', label="default")
+            plt.plot(x, y_o, 'b', label="optimised")
+            plt.xlabel("n")
+            plt.ylabel("Average time")
+            plt.title(f"Plot time opt up lvl {up_}")
+            plt.savefig(f"Plot time opt up lvl {up_}")
+            # plt.show()
+            plt.close()
+        for count_ in ["01000", "10000"]:
+            x = [x[0] for x in data if x[1] == count_]
+            y_d = [float(x[4]) for x in data if x[1] == count_]
+            y_o = [float(x[5]) for x in data if x[1] == count_]
+            plt.plot(x, y_d, 'g', label="default")
+            plt.plot(x, y_o, 'b', label="optimised")
+            plt.xlabel("k")
+            plt.ylabel("Average time")
+            plt.title(f"Plot time opt count {count_}")
+            plt.savefig(f"Plot time opt count {count_}")
+            # plt.show()
+            plt.close()
